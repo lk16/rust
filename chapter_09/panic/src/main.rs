c83b9a5f860bd9fbc013ad3539b4b834028294cd
Luuk Verweij <luuk_verweij@msn.com>
2020-02-16 20:37:31 +0100

    feat: add chapter 09

---
+++ chapter_09/panic/src/main.rs
@@ -0,0 +1,28 @@
+use std::fs::File;
+use std::io::{self, ErrorKind, Read};
+
+fn main() {
+
+    let f = File::open("hello.txt");
+
+    let _f = match f {
+        Ok(file) => file,
+        Err(error) => match error.kind() {
+            ErrorKind::NotFound => match File::create("hello.txt") {
+                Ok(fc) => fc,
+                Err(e) => panic!("Problem creating the file: {:?}", e),
+            },
+            other_error => panic!("Problem opening the file: {:?}", other_error),
+        },
+    };
+
+    let _r = read_username_from_file();
+}
+
+fn read_username_from_file() -> Result<String, io::Error> {
+    let mut s = String::new();
+
+    File::open("hello.txt")?.read_to_string(&mut s)?;
+
+    Ok(s)
+}
