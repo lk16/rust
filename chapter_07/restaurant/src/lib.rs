989ceb52930a50d34560d2c60ee0e5bd834e0812
Luuk Verweij <luuk_verweij@msn.com>
2020-02-16 13:10:28 +0100

    feat: add chapter 07

---
+++ chapter_07/restaurant/src/lib.rs
@@ -0,0 +1,58 @@
+use std::io::{self, Write};
+
+mod front_of_house;
+
+pub use crate::front_of_house::hosting;
+
+pub fn eat_at_restaurant() {
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+    hosting::add_to_waitlist();
+}
+
+mod back_of_house {
+    fn fix_incorrect_order() {
+        cook_order();
+    }
+
+    fn cook_order() {}
+
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
+
+    pub enum Appetizer {
+        Soup,
+        Salad,
+    }
+}
+
+pub fn bar() {
+    // Absolute path
+    crate::front_of_house::hosting::add_to_waitlist();
+
+    // Relative path
+    front_of_house::hosting::add_to_waitlist();
+
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    println!("I'd like {} tost please", meal.toast);
+
+    let order1 = back_of_house::Appetizer::Soup;
+    let order2 = back_of_house::Appetizer::Salad;
+}
+
+use back_of_house::Appetizer;
+
+pub fn foo() {
+    let order1 = Appetizer::Soup;
+}
--- run_all.sh
+++ run_all.sh
@@ -7,10 +7,15 @@ rustc main.rs
 ./main
 cd ../..
 
-for dir in `ls -d ./*/* | grep -v hello_world`
+for dir in `ls -d ./*/* | grep -v hello_world | grep -v restaurant`
 do
     echo "running code in $dir"
     cd $dir
     cargo run
     cd ../..
 done
+
+echo "checking code in chapter07/restaurant"
+cd chapter_07/restaurant
+cargo check
+cd ../..
